c57082a7f7ff7ec0862dc022cbb2752de32e4389
firoz.nur <firoznur5@gmail.com>
2025-04-06 08:01:05 +0600

    Add : Digital Product

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { Product } from "./models/01-Product";
+import { DigitalProduct } from "./models/02-DigitalProduct";
 
 const sampleProduct = new Product(1, "Demo Product", 20, 50);
 console.log(sampleProduct.getDetails());
 console.log(sampleProduct.getStockValue());
+
+const sampleDigitalProduct = new DigitalProduct(1, "Demo Digital Product", 20, 50, 100);
+console.log(sampleDigitalProduct.getDetails());
+console.log(sampleDigitalProduct.getStockValue());
+console.log(sampleDigitalProduct.getDownloadInfo());
